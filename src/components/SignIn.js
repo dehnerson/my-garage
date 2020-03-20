d88890edc4160357748577d8b127049ea9edb03e
AA07510 <HiGmniSDga1305>
2020-03-20 15:07:05 +0100

    Formatted SignIn

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -81,7 +81,7 @@ class SignIn extends Component {
               <Avatar className={classes.avatar}>
                 <LockOutlinedIcon />
               </Avatar>
-      <Typography component="h1" variant="h5">{t('signIn')}</Typography>
+              <Typography component="h1" variant="h5">{t('signIn')}</Typography>
               <form className={classes.form} noValidate>
                 <TextField variant="outlined" margin="normal" required fullWidth id="email"
                   label={t('emailAdress')} name="email" autoComplete="email" autoFocus
@@ -103,7 +103,7 @@ class SignIn extends Component {
                     <Link href="#" variant="body2">{t('forgotPassword')}</Link>
                   </Grid>
                   <Grid item>
-                <Link href="#" variant="body2">{t('noAccountSignUp')}</Link>
+                    <Link href="#" variant="body2">{t('noAccountSignUp')}</Link>
                   </Grid>
                 </Grid>
                 <Button onClick={this.handleGoogleSignin}>{t('signInWithGoogle')}</Button>
