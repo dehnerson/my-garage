0552299b2dc056e61bc6c838ae34ee05c6d53a97
Sascha Dehler <saschadehler@gmail.com>
2020-08-10 11:29:59 +0200

    converted Profile to functional comp

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from "react-redux";
+import { useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/styles';
 import { Paper, Avatar, Typography } from '@material-ui/core';
+import AddTextFieldsMaintenance from './AddFieldsMaintenance';
 
 const useStyles = makeStyles(theme => ({
-    root: {
+    paper: {
         alignItems: 'center',
         padding: theme.spacing(2, 0, 2, 0)
+    },
+    spacer: {
+        padding: theme.spacing(1, 0)
     },
     avatar: {
         width: 60,
@@ -19,25 +23,23 @@ const useStyles = makeStyles(theme => ({
 }));
 
 
-const Profile = (props) => {
+const Profile = () => {
     const classes = useStyles();
 
-    const { profile } = props;
+    const profile = useSelector(state => state.firebase.profile);
 
     return (
-        <Paper className={classes.root}>
-            <Avatar alt="Person" className={classes.avatar} component={Link} src={profile.avatarUrl} to="/profile" />
-            <Typography className={classes.name} variant="h6">{profile.displayName}</Typography>
-            <Typography variant="body2" color="textSecondary">{profile.email}</Typography>
-        </Paper>
+        <div>
+            <Paper className={classes.paper}>
+                <Avatar alt="Person" className={classes.avatar} component={Link} src={profile.avatarUrl} to="/profile" />
+                <Typography className={classes.name} variant="h6">{profile.displayName}</Typography>
+                <Typography variant="body2" color="textSecondary">{profile.email}</Typography>
+            </Paper>
+            <div className={classes.spacer} />
+            <AddTextFieldsMaintenance />
+        </div>
     );
 };
 
 
-const mapStateToProps = (state) => {
-    return {
-        profile: state.firebase.profile
-    };
-}
-
-export default connect(mapStateToProps)(Profile);
+export default Profile;
